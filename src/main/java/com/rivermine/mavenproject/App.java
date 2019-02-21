e69978062365e4641ba6c4484fc140e70f97fcfb
lamkhade <47698352+lamkhade@users.noreply.github.com>
2019-02-21 14:02:33 +0530

    Update App.java

--- src/main/java/com/rivermine/mavenproject/App.java
+++ src/main/java/com/rivermine/mavenproject/App.java
@@ -8,6 +8,6 @@ public class App
     {
 public static void main( String[] args )
 {
-System.out.println( "Hello World Ravindra !" );
+System.out.println( "Hello World Dear Ravindra !" );
           }
 }
